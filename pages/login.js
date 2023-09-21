c7afae7de4f7ae1fed98f51dadc4eadcab9e3a67
Ahmet Ergül <shiftusu2@gmail.com>
2023-09-21 10:28:48 +0300

    pocketbase signup added

--- pages/login.js
+++ pages/login.js
@@ -12,7 +12,7 @@ import {
   AlertTitle,
   Button,
 } from "@mui/material";
-import {LoadingButton} from '@mui/lab';
+import { LoadingButton } from "@mui/lab";
 import { useRouter } from "next/router";
 import { useDispatch, useSelector } from "react-redux";
 import { bindActionCreators } from "redux";
@@ -28,11 +28,13 @@ import CustomInput from "../components/custom-mui/CustomInput";
 import dynamic from "next/dynamic";
 import NoSsr from "../components/NoSsr";
 import Navbar from "../components/navbar/Navbar";
+import PocketBase from "pocketbase";
 
+const pb = new PocketBase(process.env.NEXT_PUBLIC_POCKET_BASE_KEY);
 
-const darkTheme= createTheme({
+const darkTheme = createTheme({
   palette: {
-      mode: "dark",
+    mode: "dark",
   },
 });
 
@@ -42,7 +44,6 @@ const LoginComponent = ({ onTypeChange = () => {} }) => {
   const dispatch = useDispatch();
   const { setCurrentUser } = bindActionCreators(actionCreators, dispatch);
 
-  
   const router = useRouter();
 
   const [password, setPassword] = useState("");
@@ -67,27 +68,31 @@ const LoginComponent = ({ onTypeChange = () => {} }) => {
       })
       .then((e) => {
         const token = e.data.token;
-        axios.get(process.env.NEXT_PUBLIC_FIND_USER + e.data.id, {
-          headers: {'Content-Type': 'application/json', "Authorization": token}
-        }).then((ele)=>{
-          setCurrentUser({...ele.data, token});
-          router.push("/events");
-        })
+        axios
+          .get(process.env.NEXT_PUBLIC_FIND_USER + e.data.id, {
+            headers: {
+              "Content-Type": "application/json",
+              Authorization: token,
+            },
+          })
+          .then((ele) => {
+            setCurrentUser({ ...ele.data, token });
+            router.push("/events");
+          });
       });
   };
 
   return (
     <NoSsr>
-      <Modal
-        open={open}
-        onClose={() => setOpen(false)}
-      >
+      <Modal open={open} onClose={() => setOpen(false)}>
         <div className={styles.forgot_my_password}>
           <h2>Şifreni mi unuttun?</h2>
-          <p>Değiştirmende yardımcı olalım. Aşağıda girdiğin e-posta adresine gelen linke tıkla. {"(Spam kutusunu kontrol etmeyi unutma.)"}</p>
+          <p>
+            Değiştirmende yardımcı olalım. Aşağıda girdiğin e-posta adresine
+            gelen linke tıkla. {"(Spam kutusunu kontrol etmeyi unutma.)"}
+          </p>
           <ThemeProvider theme={darkTheme}>
-
-            <TextField 
+            <TextField
               variant="outlined"
               className={styles.recovery_email_input}
               placeholder="E-posta adresin"
@@ -96,26 +101,35 @@ const LoginComponent = ({ onTypeChange = () => {} }) => {
               onChange={(e) => {
                 setRecoveryEmail(e.target.value);
               }}
-            >
-            </TextField>
-            
-              <LoadingButton
-                variant="outlined"
-                loading={loading}
-                onClick={() => {
-                  setLoading(true);
-                  axios.post("https://yzt-backend.vercel.app/users/resetPassword", {
+            ></TextField>
+
+            <LoadingButton
+              variant="outlined"
+              loading={loading}
+              onClick={() => {
+                setLoading(true);
+                axios
+                  .post("https://yzt-backend.vercel.app/users/resetPassword", {
                     UserEmail: recoveryEmail,
                     DateNow: new Date(),
-                    Expire: 3600000
-                  }).then((e) => {console.log("successfull"); setOpen(false); setLoading(false)}).catch(err=>{console.log(err); setLoading(false);alert("İşlem başarısız oldu.")});
-                }}
-                className={styles.recovery_email_button}
-                size="medium"
-              >
-                GÖNDER
-              </LoadingButton>
-            
+                    Expire: 3600000,
+                  })
+                  .then((e) => {
+                    console.log("successfull");
+                    setOpen(false);
+                    setLoading(false);
+                  })
+                  .catch((err) => {
+                    console.log(err);
+                    setLoading(false);
+                    alert("İşlem başarısız oldu.");
+                  });
+              }}
+              className={styles.recovery_email_button}
+              size="medium"
+            >
+              GÖNDER
+            </LoadingButton>
           </ThemeProvider>
         </div>
       </Modal>
@@ -143,9 +157,7 @@ const LoginComponent = ({ onTypeChange = () => {} }) => {
           <p onClick={typeHandler}>
             Hesabın yok mu? <strong>Kayıt ol.</strong>
           </p>
-          <p
-            onClick={() => setOpen(true)}  
-          >
+          <p onClick={() => setOpen(true)}>
             Şifreni mi unuttun? <strong>Yardım al.</strong>
           </p>
           <button className={styles.button} onClick={onLogin}>
@@ -176,9 +188,25 @@ const RegisterComponent = ({ onTypeChange }) => {
 
   const [errorPopup, setErrorPopup] = useState(false);
 
-  const onRegsiter = () => {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi;
-    if(re.test(String(email).toLowerCase())){
+  const onRegsiter = async () => {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/gi;
+    if (re.test(String(email).toLowerCase())) {
+      try {
+        const record = await pb.collection("users").create({
+          email: email.trim(),
+          name: name.trim(),
+          username: email.trim(),
+          emailVisibility: true,
+          password: password.trim(),
+          passwordConfirm: password.trim(),
+          admin: false,
+          activeMember: false
+        });
+        if(record) console.log("pocketbase signup successfull");
+      } catch (error) {
+        console.log("pocketbase signup failed, please contact authorities", error)
+      }
       axios
         .post(
           process.env.NEXT_PUBLIC_REGISTER,
@@ -196,24 +224,29 @@ const RegisterComponent = ({ onTypeChange }) => {
         )
         .then((e) => {
           axios
-          .post(process.env.NEXT_PUBLIC_LOGIN, {
-            Email: email,
-            Password: password,
-          })
-          .then((e) => {
-            const token = e.data.token;
-            axios.get(process.env.NEXT_PUBLIC_FIND_USER + e.data.id, {
-              headers: {'Content-Type': 'application/json', "Authorization": token}
-            }).then((ele)=>{
-              setCurrentUser({...ele.data, token});
-              router.push("/events");
+            .post(process.env.NEXT_PUBLIC_LOGIN, {
+              Email: email,
+              Password: password,
             })
-          });
+            .then((e) => {
+              const token = e.data.token;
+              axios
+                .get(process.env.NEXT_PUBLIC_FIND_USER + e.data.id, {
+                  headers: {
+                    "Content-Type": "application/json",
+                    Authorization: token,
+                  },
+                })
+                .then((ele) => {
+                  setCurrentUser({ ...ele.data, token });
+                  router.push("/events");
+                });
+            });
         });
-    }else{
+    } else {
       setErrorPopup(true);
-      setTimeout(()=>{
-          setErrorPopup(false);
+      setTimeout(() => {
+        setErrorPopup(false);
       }, 2000);
     }
   };
@@ -222,15 +255,23 @@ const RegisterComponent = ({ onTypeChange }) => {
     <NoSsr>
       <Modal
         open={errorPopup}
-        onClose={()=>{setErrorPopup(false)}}
+        onClose={() => {
+          setErrorPopup(false);
+        }}
       >
         <div
-            style={{width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center"}}
+          style={{
+            width: "100vw",
+            height: "100vh",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
         >
-            <Alert severity="error" style={{width: "28.5vw"}}>
-                <AlertTitle>Hata!</AlertTitle>
-                Bilgilerinizi kontrol edin veya yönetimle iletişime geçin.
-            </Alert>
+          <Alert severity="error" style={{ width: "28.5vw" }}>
+            <AlertTitle>Hata!</AlertTitle>
+            Bilgilerinizi kontrol edin veya yönetimle iletişime geçin.
+          </Alert>
         </div>
       </Modal>
       <div className={styles.register__wrapper}>
